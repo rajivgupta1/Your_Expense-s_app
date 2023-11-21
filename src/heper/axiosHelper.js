8531eee5cd98492d75fd8672e7b2a222195fdd94
Rajiv Kumar Gupta <100814533+rajivgupta1@users.noreply.github.com>
2023-11-21 15:42:17 +1100

    your_expenses

---
+++ src/heper/axiosHelper.js
@@ -0,0 +1,116 @@
+import axios from "axios";
+
+const rootApi = process.env.REACT_APP_ROOTAPI;
+const userApi = rootApi + "/user";
+const transApi = rootApi + "/transaction";
+
+const getUserId = () => {
+  const userJson = sessionStorage.getItem("user");
+  const userObj = JSON.parse(userJson);
+  return userObj?._id || null;
+};
+
+// ============= user api
+
+export const postUser = async (userObj) => {
+  try {
+    console.log(userApi);
+    const { data } = await axios.post(userApi, userObj);
+
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      mesage: error.message,
+    };
+  }
+};
+
+//login user
+export const loginUser = async (userObj) => {
+  try {
+    const { data } = await axios.post(userApi + "/login", userObj);
+
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      mesage: error.message,
+    };
+  }
+};
+
+// ============= transaction api
+
+export const postTrans = async (transObj) => {
+  try {
+    const userId = getUserId();
+    if (!userId) {
+      return {
+        status: "error",
+        message: "userId not foud, log out and log in again.",
+      };
+    }
+    const { data } = await axios.post(transApi, transObj, {
+      headers: {
+        Authorization: userId,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      mesage: error.message,
+    };
+  }
+};
+
+export const getTrans = async () => {
+  try {
+    const userId = getUserId();
+    if (!userId) {
+      return {
+        status: "error",
+        message: "userId not foud, log out and log in again.",
+      };
+    }
+    const { data } = await axios.get(transApi, {
+      headers: {
+        Authorization: userId,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      mesage: error.message,
+    };
+  }
+};
+
+export const deleteTrans = async (idArg) => {
+  try {
+    const userId = getUserId();
+    if (!userId) {
+      return {
+        status: "error",
+        message: "userId not foud, log out and log in again.",
+      };
+    }
+    const { data } = await axios.delete(transApi, {
+      data: idArg,
+      headers: {
+        Authorization: userId,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      mesage: error.message,
+    };
+  }
+};
